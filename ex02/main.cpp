efce8cbf05f831bbb9467b2d540a56d8937588d6
ocviller <ocviller@student.42.fr>
2026-02-06 10:44:50 +0100

    ex02 fix post increm

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,66 +6,11 @@
 /*   By: ocviller <ocviller@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/05 10:57:51 by ocviller          #+#    #+#             */
-/*   Updated: 2026/01/10 19:29:07 by ocviller         ###   ########.fr       */
+/*   Updated: 2026/02/06 10:44:44 by ocviller         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
-
-// int main(void)
-// {
-//     Fixed a(10);
-//     Fixed b(3.5f);
-//     Fixed c(10);
-
-//     std::cout << "===== COMPARAISONS =====" << std::endl;
-//     std::cout << "a = " << a << ", b = " << b << ", c = " << c << std::endl;
-
-//     std::cout << "a > b  : " << (a > b) << std::endl;
-//     std::cout << "a < b  : " << (a < b) << std::endl;
-//     std::cout << "a >= c : " << (a >= c) << std::endl;
-//     std::cout << "a <= c : " << (a <= c) << std::endl;
-//     std::cout << "a == c : " << (a == c) << std::endl;
-//     std::cout << "a != b : " << (a != b) << std::endl;
-
-//     std::cout << "\n===== ARITHMETIQUE =====" << std::endl;
-
-//     Fixed add = a + b;
-//     Fixed sub = a - b;
-//     Fixed mul = a * b;
-//     Fixed div = a / b;
-
-//     std::cout << "a + b = " << add << std::endl;
-//     std::cout << "a - b = " << sub << std::endl;
-//     std::cout << "a * b = " << mul << std::endl;
-//     std::cout << "a / b = " << div << std::endl;
-
-//     std::cout << "\n===== INCREMENT / DECREMENT =====" << std::endl;
-
-//     Fixed x(1);
-
-//     std::cout << "x       = " << x << std::endl;
-//     std::cout << "++x     = " << ++x << std::endl;
-//     std::cout << "x       = " << x << std::endl;
-//     std::cout << "x++     = " << x++ << std::endl;
-//     std::cout << "x       = " << x << std::endl;
-//     std::cout << "--x     = " << --x << std::endl;
-//     std::cout << "x--     = " << x-- << std::endl;
-//     std::cout << "x       = " << x << std::endl;
-
-//     std::cout << "\n===== MIN / MAX =====" << std::endl;
-
-//     std::cout << "min(a, b) = " << Fixed::min(a, b) << std::endl;
-//     std::cout << "max(a, b) = " << Fixed::max(a, b) << std::endl;
-
-//     const Fixed d(42.42f);
-//     const Fixed e(21.21f);
-
-//     std::cout << "min(d, e) = " << Fixed::min(d, e) << std::endl;
-//     std::cout << "max(d, e) = " << Fixed::max(d, e) << std::endl;
-
-//     return 0;
-// }
 
 int main(void)
 {
